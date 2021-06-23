6dbee39cb968c3387b2b8339a5a628bef83f8e01
Nichlas <nichlasd@gmail.com>
2021-06-23 13:13:43 +0200

    add Lottery.sol from remix and add a test

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+    'clip culture later brother elephant april obtain subject walk ahead stomach satoshi',
+    'https://rinkeby.infura.io/v3/3936ce115f3a4803afe71b17f72bdea1'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
